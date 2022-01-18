b84590180a90008a87854aa31bf969c30b87fdb7
Nuruddin 123 <mnoervandores@gmail.com>
2022-01-18 15:51:18 +0700

    Initial commit

---
+++ resources/views/includes/main/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+  <div class="footer-left">
+    &copy; TREEHOTEL 2021 - Present by <a href="https://fajarwz.netlify.app" target="_blank">Bismillah</a> <div class="bullet"></div> Powered by BarengSaya &copy; 2022 by <a href="https://nauval.in/" target="_blank">NoerV</a>
+  </div>
+  <div class="footer-right">
+    1.0
+  </div>
+</footer>
